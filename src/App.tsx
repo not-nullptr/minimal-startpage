e39087238b172962b8fc4bf580adeb7494d3b9f1
madeline-xoxo <needhelpwithrift@gmail.com>
2023-05-05 20:51:59 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+function App() {
+	return (
+		<div className="main">
+			<div className="container">
+				<h1>welcome back!</h1>
+				<div>missed me? i know {"<3"}</div>
+				<input
+					onKeyUp={(e) => {
+						if (e.key !== "Enter") return;
+						e.preventDefault();
+						window.location.href = `https://google.com/search?q=${
+							(e.target as HTMLInputElement).value
+						}`;
+					}}
+					type="search"
+					placeholder="search google for..."
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default App;
